000b769287193c704b370155d7ffb32bc06ea074
vect0r <denisgaiciuk@gmail.com>
2020-06-04 11:05:00 +0300

    It's our first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+'use strict'
+let elem = document.getElementById('but');
+addEventListener('click', function(event) {
+// document.getElementById('text').hidden = true; 
+  document.getElementById('text').style.display = 'none';
+})
